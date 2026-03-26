de193e6f607ec10c83b30b840a1780adeb9ced62
Jinsu Hong <jinsuhong.knight@gmail.com>
2026-03-25 23:23:30 -0400

    update check_zarr.py

--- tmp/check_zarr.py
+++ tmp/check_zarr.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     data_path  = "/scratch/users/jhong36/data/sdo_512.zarr"
 
     # Open a Zarr dataset lazily
-    ds = xr.open_zarr(data_path, chunks={})
+    ds = xr.open_zarr(data_path, chunks="auto")
 
     # Print Data variables
     print(ds.data_vars)
