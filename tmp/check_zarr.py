1eba6e9bf4e60e92ad9530dfa8c6b4aac3c78f35
Jinsu Hong <jinsuhong.knight@gmail.com>
2026-03-27 22:56:24 -0400

    create script for computing statistics of zarr data

--- tmp/check_zarr.py
+++ tmp/check_zarr.py
@@ -1,16 +1,25 @@
+from pprint import pprint
+
 import xarray as xr
 
 if __name__ == "__main__":
 
-    data_path  = "/scratch/users/jhong36/data/sdo_512.zarr"
+    data_path  = "/media/jhong90/storage/surya/sdo_512.zarr/2010"
 
     # Open a Zarr dataset lazily
-    ds = xr.open_zarr(data_path, chunks="auto")
+    ds = xr.open_zarr("/media/jhong90/storage/surya/sdo_512.zarr", group="2010/hmi_m", chunks="auto")
+
+    # print all the variables and coords
+    print(ds.variables)
 
     # Print Data variables
     print(ds.data_vars)
 
     # Print coordinates
     print(ds.coords)
+
+    image = ds["hmi_m"].sel(timestep="2010-05-13T01:00:00").values
+    pprint(image)
+
 
 
