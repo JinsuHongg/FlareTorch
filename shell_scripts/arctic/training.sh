431b4f2600f9ea8f58db8f63a0886258a2970c2d
jhong36 <jhong36@arclogin04.rs.gsu.edu>
2026-05-03 03:37:04 +0000

    update shell script for training

--- shell_scripts/arctic/training.sh
+++ shell_scripts/arctic/training.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-#SBATCH -p qCPU24
+#SBATCH -p qGPU24
 #SBATCH -A csc344r253
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=2
 #SBATCH --time=01:00:00
 #SBATCH --mem=64G
-#SBATCH --job-name=my_job
-#SBATCH --output=job_%j.out
-#SBATCH --error=job_%j.err
+#SBATCH --job-name=uqtraining
+#SBATCH --output=training_%j.out
+#SBATCH --error=training_%j.err
 
 # Activate your Python environment
-source .venv/bin/activate
-
-cd /home/jhong90/github_proj/FlareTorch
+source /home/users/jhong36/miniforge3/etc/profile.d/conda.sh
+cd /home/users/jhong36/projects/FlareTorch
+conda activate flaretorch
 
 python -u scripts/training.py
